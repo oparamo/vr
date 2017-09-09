dcf45719f58ed904ce1ab3583f8650f5dd2e51e6
Oscar Paramo <oscar@vin.li>
2017-09-09 08:44:04 -0500

    Use template component.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 'use strict';
 
-console.log('do cool stuff');
+const aframe = require('aframe');
+require('aframe-template-component');
+
+aframe.registerPrimitive('a-stage', require('./primitives/a-stage'));
+aframe.registerComponent('stagehand', require('./components/stagehand'));
